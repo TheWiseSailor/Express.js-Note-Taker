ccd215ce3d22eb37af60d40ec935fb05bade338c
TheWiseSailor <rudasilljay@gmail.com>
2023-07-04 14:19:38 -0400

    something worked since i changed paths and added the get and remove notes feature in the js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,31 +1,41 @@
-// Declare router variable and store variable express.Router() and db store 
-// Require express and router
-const router = require("express").Router();
-const store = require("../db/db.json");
+const express = require('express');
+const router = express.Router();
+const fs = require('fs');
 const path = require('path');
-// request existing notes from the db.json file
-router.get("/notes", function (_req, res) {
-    // get notes from the db.json file
-    res.sendFile(path.join(__dirname, '../db/db.json'))
 
-//  store.getNotes()
-//         // send notes if there are no errors
-//         .then(notes => res.json(notes))
-//         // send err if there is an error
-//         .catch(err => res.status(500).json(err));
+// Define the path to the db.json file
+const dbPath = path.join(__dirname, '..', 'db', 'db.json');
+
+// Read the notes from the db.json file
+function getNotes() {
+  return JSON.parse(fs.readFileSync(dbPath, 'utf8'));
+}
+
+// Save the notes to the db.json file
+function saveNotes(notes) {
+  fs.writeFileSync(dbPath, JSON.stringify(notes));
+}
+
+router.get('/notes', (req, res) => {
+  const notes = getNotes();
+  res.json(notes);
 });
-// posting note function route
-router.post("/notes", (req, res) => {
-    console.log(req.body);
-    store.addNote(req.body)
-        .then(note => res.json(note))
-        .catch(err => res.status(500).json(err))
+
+router.post('/notes', (req, res) => {
+  const newNote = req.body;
+  const notes = getNotes();
+  newNote.id = Date.now().toString();
+  notes.push(newNote);
+  saveNotes(notes);
+  res.json(newNote);
 });
-// delete note function route
-router.delete("/notes/:id", (req, res) => {
-    store.removeNote(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch(err => res.status(500).json(err))
+
+router.delete('/notes/:id', (req, res) => {
+  const noteId = req.params.id;
+  const notes = getNotes();
+  const updatedNotes = notes.filter((note) => note.id !== noteId);
+  saveNotes(updatedNotes);
+  res.json({ message: 'Note deleted successfully.' });
 });
-// export router
+
 module.exports = router;
